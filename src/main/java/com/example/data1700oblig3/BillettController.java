810d96f87192539aea7ec2435280318e41126387
Capt-Obv <49240674+Capt-Obv@users.noreply.github.com>
2024-03-26 14:36:48 +0100

    Laget funksjonalitet for å lagre billett på databasen

--- src/main/java/com/example/data1700oblig3/BillettController.java
+++ src/main/java/com/example/data1700oblig3/BillettController.java
@@ -1,4 +1,18 @@
 package com.example.data1700oblig3;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+@RestController
 public class BillettController {
+    @Autowired
+    private BillettRepository rep;
+    public void lagreBillett(Billett billett, HttpServletResponse response) throws IOException {
+        if(!rep.lagreBillett(billett)){
+            response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Feil i db prøv senere!");
+        }
+    }
 }
